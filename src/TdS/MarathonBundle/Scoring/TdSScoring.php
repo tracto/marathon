90030fd305985bc05977d0b4de3a2ef698bbe747
tiretdusix <contact@tiretdusix.net>
2016-04-05 08:53:08 +0200

    Wall of Fame : Ajout dans l'index + dans le service de scores

--- src/TdS/MarathonBundle/Scoring/TdSScoring.php
+++ src/TdS/MarathonBundle/Scoring/TdSScoring.php
@@ -1,7 +1,24 @@
 <?php
 namespace TdS\MarathonBundle\Scoring;
 
+use TdS\MarathonBundle\Entity\Joggeur;
+use TdS\MarathonBundle\Entity\JoggeurScore;
+use Doctrine\ORM\EntityManager;
+
 class TdSScoring {
+
+	private $em;
+
+	/**
+	 * @InjectParams({
+	 *    "em" = @Inject("doctrine.orm.entity_manager")
+	 * })
+	 */
+	public function __construct(EntityManager $em)
+	{
+	    $this->em = $em;
+	}
+
 	public function getTotalList($list){
 		$sum=0;
 		foreach($list as $item){
@@ -24,4 +41,30 @@ class TdSScoring {
     	usort($list, array($this,"compareScorebyTotal"));
     	return $list;
     }
+
+
+
+    
+
+    public function getIdJogFame($listeJoggeursScore,$col,$sort){
+    	$filtre='get'.$col;
+    	$jogFameArray=array();
+    	foreach($listeJoggeursScore as $joggeurScore){
+    		$totalFame=0;
+    		foreach($joggeurScore->getScores() as $score){
+    			$totalFame+=$score->$filtre();   			
+    		}
+    		$jogFameArray[$joggeurScore->getJoggeur()->getId()]=$totalFame;   		
+    	}
+
+    	$sort($jogFameArray);
+
+	    $idJoggeur=array_keys($jogFameArray)[0];
+	    $wof_jogFame = $this->em
+		          ->getRepository('TdSMarathonBundle:Joggeur')
+		          ->findOneBy(array('id'=>$idJoggeur));
+
+    	return $wof_jogFame;
+    }
+
 }
